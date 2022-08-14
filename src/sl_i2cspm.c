f84e1fcb5abf7609f699bb160599c147e756fc2f
CongDo <danhconget@gmail.com>
2022-08-14 22:11:03 +0700

    commit test2

---
+++ src/sl_i2cspm.c
@@ -0,0 +1,6 @@
+
+#include "sl_i2cspm.h"
+
+I2C_TransferReturn_TypeDef I2CSPM_Transfer(I2C_TypeDef *i2c, I2C_TransferSeq_TypeDef *seq) {
+    return i2cTransferDone;
+}
---
+++ src/ut_id12la.c
@@ -0,0 +1,32 @@
+#include "rfid_id12la.h"
+
+static void id12la_tc_000(void)
+{
+	sl_status_t status;
+	sl_i2cspm_t i2cspm;
+
+	status = id12la_init(&i2cspm);
+
+	status = id12la_init(NULL);
+}
+
+static void id12la_tc_001(void)
+{
+	uint8_t new_address;
+	sl_status_t status;
+	status = id12la_change_address_i2c(new_address);
+}
+
+void id12la_ts_000(void)
+{
+	id12la_tc_000();
+
+	id12la_tc_001();
+}
+
+int main(void)
+{
+  id12la_ts_000();
+
+  return 0;
+}
